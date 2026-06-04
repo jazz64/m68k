a091f0be1757c8248c51668c000acc7fca966539
Markus-Schahin Bollerey <markus@bollerey.de>
2026-06-04 15:13:26 +0200

    Fixes ANDI-instruction registration

--- src/main/java/m68k/cpu/instructions/ANDI.java
+++ src/main/java/m68k/cpu/instructions/ANDI.java
@@ -1,6 +1,9 @@
 package m68k.cpu.instructions;
 
 import m68k.cpu.*;
+import m68k.cpu.rules.AddressingMode;
+
+import static m68k.cpu.rules.AddressingMode.dataAlterableModes;
 
 /*
 //  M68k - Java Amiga MachineCore
@@ -40,64 +43,57 @@ public class ANDI implements InstructionHandler
 		int base;
 		Instruction i;
 
-		for(int sz = 0; sz < 3; sz++)
+		for (Size sz : Size.values())
 		{
-			if(sz == 0)
-			{
-				// andi byte
-				base = 0x0200;
-				i = new Instruction() {
-					public int execute(int opcode)
-					{
-						return andi_byte(opcode);
-					}
-					public DisassembledInstruction disassemble(int address, int opcode)
-					{
-						return disassembleOp(address, opcode, Size.Byte);
-					}
-				};
-			}
-			else if(sz == 1)
-			{
-				// andi word
-				base = 0x0240;
-				i = new Instruction() {
-					public int execute(int opcode)
-					{
-						return andi_word(opcode);
-					}
-					public DisassembledInstruction disassemble(int address, int opcode)
-					{
-						return disassembleOp(address, opcode, Size.Word);
-					}
-				};
-			}
-			else
-			{
-				// andi long
-				base = 0x0280;
-				i = new Instruction() {
-					public int execute(int opcode)
-					{
-						return andi_long(opcode);
-					}
-					public DisassembledInstruction disassemble(int address, int opcode)
-					{
-						return disassembleOp(address, opcode, Size.Long);
-					}
-				};
-			}
-			for(int ea_mode = 0; ea_mode < 8; ea_mode++)
-			{
-				if(ea_mode == 1)
-					continue;
+			switch (sz) {
+				case Byte -> {
+					// andi byte
+					base = 0x0200;
+					i = new Instruction() {
+						@Override
+						public int execute(int opcode) {
+							return andi_byte(opcode);
+						}
 
-				for(int ea_reg = 0; ea_reg < 8; ea_reg++)
-				{
-					if(ea_mode == 7 && (ea_reg == 2 || ea_reg == 3 || ea_reg == 4))
-						continue;
-					is.addInstruction(base + (ea_mode << 3) + ea_reg, i);
+						@Override
+						public DisassembledInstruction disassemble(int address, int opcode) {
+							return disassembleOp(address, opcode, sz);
+						}
+					};
 				}
+				case Word -> {
+					// andi word
+					base = 0x0240;
+					i = new Instruction() {
+						@Override
+						public int execute(int opcode) {
+							return andi_word(opcode);
+						}
+
+						@Override
+						public DisassembledInstruction disassemble(int address, int opcode) {
+							return disassembleOp(address, opcode, sz);
+						}
+					};
+				}
+				default -> {
+					// andi long
+					base = 0x0280;
+					i = new Instruction() {
+						@Override
+						public int execute(int opcode) {
+							return andi_long(opcode);
+						}
+
+						@Override
+						public DisassembledInstruction disassemble(int address, int opcode) {
+							return disassembleOp(address, opcode, sz);
+						}
+					};
+				}
+			}
+			for (AddressingMode ea : dataAlterableModes()) {
+				is.addInstruction(base + (ea.getMode() << 3) + ea.getRegister(), i);
 			}
 		}
 	}
